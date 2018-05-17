adf69b7f1c9d4ffd4cfdc554883f24662ad7f949
Shikhar Bakhda <sbakhda@cognitivescale.edu>
2018-05-17 13:35:00 -0500

    successfully deployed and tested

--- skills/headline/deploy-skill.sh
+++ skills/headline/deploy-skill.sh
@@ -3,4 +3,4 @@ set -e
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Deploy our skill to Cortex
-cortex skills save --yaml headline.yaml
+cortex skills save --yaml skill.yaml
--- skills/headline/src/__main__.py
+++ skills/headline/src/__main__.py
@@ -1,5 +1,4 @@
 from cortex_client import InputMessage, OutputMessage
-from news import news
 
 def main(params):
     # Parse the function params
@@ -16,4 +15,16 @@ def main(params):
     return OutputMessage.create().with_payload({'route':'headline','top_head': top_head}).to_params()
 
 
+import requests, json
 
+# source: news source from news api
+# n: number of top articles from source
+def news(source, n, api='d641992dd1f544d28409176c74802ac4'):
+
+    url = ('https://newsapi.org/v2/top-headlines?'
+           'sources='+source+
+           '&pageSize='+str(n)+
+           '&apiKey='+api)
+    response = requests.get(url).json()
+
+    return [json.dumps({'title':art['title'], 'url':art['url']}) for art in response['articles']]
