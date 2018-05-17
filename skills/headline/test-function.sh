2af7ccd407931e276341f2a29c861ed53738699e
Shikhar Bakhda <sbakhda@cognitivescale.edu>
2018-05-17 15:05:48 -0500

    all done including readme and testing

--- skills/headline/test-function.sh
+++ skills/headline/test-function.sh
@@ -2,5 +2,7 @@
 set -e
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# test the skill using test_req.json
-cortex actions invoke nh/headline --params-file "${SCRIPT_DIR}/test/test_req.json"
+# test the skills using test jsons
+cortex actions invoke nh/headline --params-file "${SCRIPT_DIR}/test/test_espn.json"
+cortex actions invoke nh/headline --params-file "${SCRIPT_DIR}/test/test_cnn.json"
+cortex actions invoke nh/headline --params-file "${SCRIPT_DIR}/test/test_cnbc.json"
